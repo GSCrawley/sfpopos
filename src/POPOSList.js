3b17f6d4c09d03dad96bca307c41c7f71e78d6f6
Gideon Crawley <gscrawley@gmail.com>
2022-07-08 13:48:58 -0700

    POPOSList component

---
+++ src/POPOSList.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+
+function POPOSList() {
+    return (
+        <div>
+            <h1>Keep your eye on this space for future content...</h1>
+            <img src={logo} />
+
+        </div>
+    )
+}
+
+export default POPOSList
+
