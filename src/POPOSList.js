fb63f89855c72d6433c03ca207093b1f5446c1d4
Gideon Crawley <gscrawley@gmail.com>
2022-07-18 12:58:37 -0700

    added id to POPOSSpace

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -5,10 +5,10 @@ import data from './sfpopos-data.json'
 
 
 function POPOSList() {
-    const spaces = data.map((obj) => {
-        const { title, address, images, hours } = obj
+    const spaces = data.map(({title, address, images, hours }, i) => {
         return (
             <POPOSSpace
+                id={i}
                 key={title}
                 address={address}
                 image={images[0]}
