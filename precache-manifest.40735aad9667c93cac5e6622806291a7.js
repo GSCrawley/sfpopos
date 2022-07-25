d2cd279c11be224e0be446eb01b24cdc80a2799e
Gideon Crawley <gscrawley@gmail.com>
2022-07-25 12:29:36 -0700

    Updates

---
+++ precache-manifest.40735aad9667c93cac5e6622806291a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e641846a302350fe7037",
+    "url": "/sfpopos/static/css/main.4b6203eb.chunk.css"
+  },
+  {
+    "revision": "e641846a302350fe7037",
+    "url": "/sfpopos/static/js/main.05aee681.chunk.js"
+  },
+  {
+    "revision": "e8db8a8ed934569444f0",
+    "url": "/sfpopos/static/js/runtime~main.b264b361.js"
+  },
+  {
+    "revision": "5ac1dcaaa069ad753b0d",
+    "url": "/sfpopos/static/js/2.fea5e2a9.chunk.js"
+  },
+  {
+    "revision": "d981f202f24b0632b19d4d7a1cfe9c36",
+    "url": "/sfpopos/index.html"
+  }
+];
